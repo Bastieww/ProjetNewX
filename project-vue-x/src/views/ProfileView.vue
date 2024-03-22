6cb5e89f839d94f908759dd5e2b37420d40ebd7c
Jérémie Serra <Jeremie.Serra@etu.univ-smb.fr>
2024-03-22 16:46:15 +0100

    profileview

--- project-vue-x/src/views/ProfileView.vue
+++ project-vue-x/src/views/ProfileView.vue
@@ -1,14 +1,32 @@
 <script setup>
+import { useUserStore } from '@/stores/user'
+import { ref, computed } from 'vue'
+const user = useUserStore()
+
 </script>
 
-
 <template>
-    <section>
-        <p>Nom</p>
-        <p>Prenom</p>
-    </section>
+    <div>
+    <h3>{{ user.name }}</h3>
+
+    <p class="price">{{ value.price }} €</p>
+
+    <ul>
+        <li v-for="star in value.score"
+        class="score">*</li>
+    </ul>
+    <img :src="'src/assets/images/' + value.image" alt="">
+
+    <p class="addToCart">
+      <button @click="cart.add(value)">Ajouter au panier</button>
+    </p>
+    <p class="mamangeCart" v-if="inCart">
+      <button @click="cart.add(value)"></button>
+    </p>
+  </div>
 </template>
 
-<style>
+
+<style scoped>
 
 </style>
