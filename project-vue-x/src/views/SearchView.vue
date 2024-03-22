a5daf3a01be31e42387ed4737172b41bba1d6816
Bastien Vincent Dekens <bastien.vincent-dekens@etu.univ-smb.fr>
2024-03-22 16:46:18 +0100

    y('y(''y(

--- project-vue-x/src/views/SearchView.vue
+++ project-vue-x/src/views/SearchView.vue
@@ -1,6 +1,18 @@
 <script setup>
+
+const users = useUserStore()
+
+const filter = ref("")
+
+const filteredUser = computed(() =>{
+  return users.theUsers.filter(
+    usertemp => usertemp.name.toLowerCase().indexOf(filter.value) != -1)
+  
+})
 </script>
 
 <template>
+<h1>Qui allez-vous rencontrer aujourd'hui ?</h1>
+<input v-model="search"/>
 
 </template>
