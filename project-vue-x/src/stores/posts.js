a6a90088567107c1580897dad26289c95d4492bb
Bastien Vincent Dekens <bastien.vincent-dekens@etu.univ-smb.fr>
2024-04-02 17:37:14 +0200

    COMMIT DE REPARATION, LA PROCHAINE FOIS JE TUE MAXIME

--- project-vue-x/src/stores/posts.js
+++ project-vue-x/src/stores/posts.js
@@ -5,6 +5,7 @@ import axios from 'axios'
 export const usePostStore = defineStore('posts', () => {
 
     const thePosts = ref([])
+    const thePostsChilds = ref([])
     const url = "http://localhost:9090/"
     function post(texte) {
 
@@ -18,10 +19,18 @@ export const usePostStore = defineStore('posts', () => {
             thePosts.value.splice(0, 0, newPost)
         })
     }
+
         axios.get(url + "posts").then(response => {
             var temp = Array.from(response.data).filter(p => p.pos_postid == null)//Enleve les posts "enfants"
             temp.sort((a,b)=>a.postid<b.postid?1:-1)
             thePosts.value = temp
+        })                                                                                                              //OOOOOUUUUOUOUUUU LE CODE GOOOFFYYYY BY BASTIEWWW
+
+        axios.get(url + "posts").then(response => {
+            var temp = Array.from(response.data).filter(p => p.pos_postid != null)//Ne garde que les posts "enfants"
+            temp.sort((a,b)=>a.postid<b.postid?1:-1)
+            thePostsChilds.value = temp
         })
-    return { thePosts, post }
+
+    return { thePosts, thePostsChilds, post }
 })
