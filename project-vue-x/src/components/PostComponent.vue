e6c97239dd225fc9aa90b72accda2304dc9d35a0
skyhonnor <maxime.ruault@etu.univ-smb.fr>
2024-04-03 09:59:19 +0200

    add like function and rt

--- project-vue-x/src/components/PostComponent.vue
+++ project-vue-x/src/components/PostComponent.vue
@@ -26,6 +26,16 @@ function answer() {
     posts.answer(newAnsweringPost.value, props.post.postid)
 }
 
+
+function like() {
+    posts.like(theUser.value.utilisateurid, props.post.postid)
+}
+
+function rt()
+{
+    posts.rt(theUser.value.utilisateurid, props.post.postid)
+}
+
 </script>
 
 <template>
@@ -81,7 +91,7 @@ function answer() {
                         d="M8.625 9.75a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm0 0H8.25m4.125 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm0 0H12m4.125 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm0 0h-.375m-13.5 3.01c0 1.6 1.123 2.994 2.707 3.227 1.087.16 2.185.283 3.293.369V21l4.184-4.183a1.14 1.14 0 0 1 .778-.332 48.294 48.294 0 0 0 5.83-.498c1.585-.233 2.708-1.626 2.708-3.228V6.741c0-1.602-1.123-2.995-2.707-3.228A48.394 48.394 0 0 0 12 3c-2.392 0-4.744.175-7.043.513C3.373 3.746 2.25 5.14 2.25 6.741v6.018Z" />
                 </svg>
             </div>
-            <div class="retweet">
+            <div class="retweet" @click="rt">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
                     stroke="currentColor" class="icon">
                     <path stroke-linecap="round" stroke-linejoin="round"
@@ -89,12 +99,14 @@ function answer() {
                 </svg>
 
             </div>
-            <div class="like">
+            <div class="like" @click="like">
+
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
                     stroke="currentColor" class="icon">
                     <path stroke-linecap="round" stroke-linejoin="round"
                         d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z" />
                 </svg>
+
             </div>
         </div>
     </div>
@@ -178,5 +190,6 @@ function answer() {
     height: 20px;
     width: 20px;
     margin-right: 10px;
+    cursor: pointer;
 }
 </style>
